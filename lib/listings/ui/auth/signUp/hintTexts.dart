dc68fee7ae7232f2a4f572953a0252db0ce0ed45
Jahnavi <jahnavisgupta@gmail.com>
2023-10-06 12:35:11 -0400

    Initial Commit

---
+++ lib/listings/ui/auth/signUp/hintTexts.dart
@@ -0,0 +1,17 @@
+// Created a file called hintTexts.dart that has different string values 
+// for the hint text in a list called list_hintText.
+
+List<String> list_hintText  = [
+    "Best restroom you have seen?",
+    "What's Your Ideal Washroom Oasis?",
+    "Fave bathroom you've ever seen?",
+    "Top-rated restroom in your life?",
+    "Preferred lavatory you've encountered?",
+    "Memorable restroom you cherish most?",
+    "What's Your Restroom Gem?",
+    "Favoutire bathroom ever used?",
+    "Which Loo Suits Your Style Best?",
+    "Your Favorite Loo Visited?",
+    "What's Your Go-To Lavatory?",
+
+];
